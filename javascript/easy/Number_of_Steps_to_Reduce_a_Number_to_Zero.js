29493e14a2c1334b509be7f8f98e7604f1bc612e
yuu <22yuu@naver.com>
2022-12-24 13:25:45 +0900

    js(easy): 1342. Number of Steps to Reduce a Number to Zero

---
+++ javascript/easy/Number_of_Steps_to_Reduce_a_Number_to_Zero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const numberOfSteps = function (num) {
+  let count = 0;
+
+  while (num > 0) {
+    count += 1;
+
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = num - 1;
+    }
+  }
+
+  return count;
+};
