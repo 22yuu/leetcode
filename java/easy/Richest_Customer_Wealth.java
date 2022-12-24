cb4c88f0c4df3d235e81cab6e79ed3e32738ab69
yuu <22yuu@naver.com>
2022-12-24 12:29:49 +0900

    java(easy): 1672. Richest Customer Wealth

---
+++ java/easy/Richest_Customer_Wealth.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class Richest_Customer_Wealth {
+	class Solution {
+
+	    public int maximumWealth(int[][] accounts) {
+	        
+	        int max = Integer.MIN_VALUE;
+
+	        for(int i = 0; i < accounts.length; i++) {
+	            int sum = 0;
+	            for(int j = 0; j < accounts[0].length; j++) {
+	                sum += accounts[i][j];
+	            }
+	            
+
+	            if( sum > max) max = sum;
+	        }
+
+	        return max;
+	        
+	    }
+	}
+}
