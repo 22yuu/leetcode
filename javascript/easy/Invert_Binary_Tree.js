6c91469d0f73d248d5324a2ee3ba5a015d7022f0
yuu <22yuu@naver.com>
2023-01-05 15:43:47 +0900

    js(easy): 226. Invert Binary Tree

---
+++ javascript/easy/Invert_Binary_Tree.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+// my code R: 38.93, M: 42.28
+var invertTree = function (root) {
+  if (root === null) return root;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  const temp = root.right;
+  root.right = root.left;
+  root.left = temp;
+
+  return root;
+};
+
+// other code
+// Recursion
+function invertTree(root) {
+  if (root == null) return root;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+}
+
+// DFS
+function invertTree(root) {
+  const stack = [root];
+
+  while (stack.length) {
+    const n = stack.pop();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      stack.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
+
+// BFS
+function invertTree(root) {
+  const queue = [root];
+
+  while (queue.length) {
+    const n = queue.shift();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      queue.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
