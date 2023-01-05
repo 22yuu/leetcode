098bb33227d5eb42ad899b08748ae281e0c2c7f0
yuu <22yuu@naver.com>
2023-01-05 16:23:51 +0900

    chore: 112. Path Sum : add other codes

--- javascript/easy/Path_Sum.js
+++ javascript/easy/Path_Sum.js
@@ -12,6 +12,7 @@
  * @return {boolean}
  */
 
+// my code R: 85%, M: 99%
 let isHas;
 
 var hasPathSum = function (root, targetSum) {
@@ -40,4 +41,43 @@ var dfs = function (node, sum, targetSum) {
   dfs(node.right, sum + node.val, targetSum);
 
   return;
+};
+
+// other code R: 99%
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+var hasPathSum = function (root, targetSum) {
+  if (!root) {
+    return false;
+  }
+
+  if (!root.left && !root.right && root.val == targetSum) {
+    return true;
+  }
+
+  let l = false;
+  let r = false;
+
+  if (root.left) {
+    l = hasPathSum(root.left, targetSum - root.val);
+    if (l) {
+      return true;
+    }
+  }
+
+  if (root.right) {
+    r = hasPathSum(root.right, targetSum - root.val);
+    if (r) {
+      return true;
+    }
+  }
+
+  return false;
 };
