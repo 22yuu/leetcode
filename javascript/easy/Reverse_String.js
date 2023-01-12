4703aa67760d28b995acacc49e119f53dc76ab25
yuu <22yuu@naver.com>
2023-01-12 17:28:43 +0900

    js(easy): 344. Reverse String

---
+++ javascript/easy/Reverse_String.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left++;
+    right--;
+  }
+};
