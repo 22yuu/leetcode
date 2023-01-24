147926618f39641395928ae01d22b49c29dcc35f
yuu <22yuu@naver.com>
2023-01-24 17:23:22 +0900

    js(medium): 34. Find First and Last Position of Element in Sorted Array

---
+++ javascript/medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  if (nums.length == 0 || nums.indexOf(target) == -1) {
+    return [-1, -1];
+  }
+
+  let first = -1;
+  let last = -1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == target) {
+      if (first == -1) {
+        first = i;
+      }
+      last = i;
+    }
+  }
+
+  return [first, last];
+};
