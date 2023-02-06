15e8a304e45d1d00d2e13ef3f7fa250c924b1bd3
yuu <22yuu@naver.com>
2023-02-06 16:24:21 +0900

    js(medium): 130. Surrounded Regions

--- javascript/medium/Surrounded_Regions.js
+++ javascript/medium/Surrounded_Regions.js
@@ -4,6 +4,7 @@
  */
 // 둘러쌓였다는 판단을 어떻게해야할까
 
+// my code... ㅅㅣㄹ패.... 왜 안되지 ㅠ
 // var solve = function(board) {
 //     const direction = [
 //         [-1, 0],
@@ -62,6 +63,7 @@
 //     return board;
 // };
 
+// other code
 var solve = function (board) {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
